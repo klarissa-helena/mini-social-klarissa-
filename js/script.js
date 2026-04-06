44e875c79c641e125a84e87f71c46eacb43281e3
klarissa-helena <klarissa.9618213@aluno.mg.gov.br>
2026-04-06 16:09:41 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
+let likeCount =0;
+let curtido= false;
 
+function curtir (){
+    if(curtido==false){
 
+    }else{
+        likeCount--;
+        curtido = false;
+        document.getElementById("likeCount"). innerText= likeCount;
+    }
+}
+document.getElementById("likeBnt").addEventListener("click, curtir");
 
+let DeslikeCount =0;
+let Descutido =false;
+function descutir (){
+    if(descurtido==false){
 
-
+        descurtirCount++;
+        Descurtir=
+    }
