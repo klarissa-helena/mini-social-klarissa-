d914ab33ec7b54cbd786ce5cb0142219a4a40a86
klarissa-helena <klarissa.9618213@aluno.mg.gov.br>
2026-04-08 12:29:29 +0000

    update

--- js/script.js
+++ js/script.js
@@ -2,21 +2,33 @@ let likeCount =0;
 let curtido= false;
 
 function curtir (){
-    if(curtido==false){
-
+    if(curtir==false){
+        CurtirCount++;
+        Curtir= true;
+        document.getElementById("deslikeCount"). innerText = CurtirCount
+    
     }else{
-        likeCount--;
-        curtido = false;
-        document.getElementById("likeCount"). innerText= likeCount;
+        curtirCount--;
+        curtir = false;
+        document.getElementById("curtirCount"). innerText= curtirCount;
     }
-}
+
 document.getElementById("likeBnt").addEventListener("click, curtir");
 
-let DeslikeCount =0;
-let Descutido =false;
-function descutir (){
-    if(descurtido==false){
 
-        descurtirCount++;
-        Descurtir=
-    }
+let DeslikeCount =0;
+let Deslike =false;
+
+ function deslike (){
+    
+    if(deslike==false){
+
+        DeslikeCount++;
+        Deslike= true;
+        document.getElementById("deslikeCount"). innerText = DeslikeCount
+    }else{
+        DeslikeCount--;
+        Deslike= false;
+    } 
+     
+document.getElementById("deslikeBnt").addEventListener("click, deslike");
