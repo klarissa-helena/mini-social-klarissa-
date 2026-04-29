d7d109f4925787cc94e3e1880f91d35719bae2ba
klarissa-helena <klarissa.9618213@aluno.mg.gov.br>
2026-04-29 16:03:44 +0000

    add controladores

--- js/script.js
+++ js/script.js
@@ -1,40 +1,66 @@
+// === ESTADO (dados da aplicação)===
+
 let likeCount = 0;
-let curtido = false; // flag booleana
+let dislikeCount = 0;
+let curtido = false;
+let descurtido = false;
+
+// === SERVICE (regras de negócio)===
 
 function curtir() {
-
- if(curtido == false){
+  if (curtido == false){
     likeCount++;
     curtido = true;
     document.getElementById("likeCount").innerText = likeCount;
- }else{
+
+    if(descurtido == true){
+      dislikeCount--;
+      descurtido = false;
+      document.getElementById("dislikeCount").innerText = dislikeCount;
+
+    }
+
+  }else{
     likeCount--;
     curtido = false;
     document.getElementById("likeCount").innerText = likeCount;
- }
+  }
 
-  
 }
-
-document.getElementById("likeBtn").addEventListener("click", curtir);
-
-let deslikeCount = 0;
-let descurtido = false; // flag booleana
 
 function descurtir() {
-
- if(descurtido == false){
-    deslikeCount++;
+  if(descurtido == false){
+    dislikeCount++;
     descurtido = true;
-    document.getElementById("deslikeCount").innerText =  deslikeCount;
- }else{
-    deslikeCount--;
+    document.getElementById("dislikeCount").innerText = dislikeCount;
+
+    if(curtido == true){
+      likeCount--;
+      curtido = false;
+      document.getElementById("likeCount").innerText = likeCount;
+    }
+
+  }
+  else{
+    dislikeCount--;
     descurtido = false;
-    document.getElementById("deslikeCount").innerText = deslikeCount;
- }
-
-  
-
+    document.getElementById("dislikeCount").innerText = dislikeCount;
+  }
 }
 
-document.getElementById("deslikeBtn").addEventListener("click", descurtir);
+//=== CONTROLLER ===(intermediação)===
+
+function clicarCurtir (){
+curtir();
+}
+
+function descurtit (){
+descurtir();
+}
+
+
+
+//=== EVENTOS ===
+
+document.getElementById("likeBtn").addEventListener("click", clicarCurtir);
+document.getElementById("dislikeBtn").addEventListener("click", descurtir);
